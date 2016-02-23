b0a52240b9eaf60a3ab1b563f30e5ad86e815ecf
tudh2 <minhtu.svptit@gmail.com>
2016-02-23 11:43:06 +0700

    routing

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -7,5 +7,15 @@ describe('my app', function() {
   beforeEach(function() {
     browser.get('app/index.html');
   });
+  });
+  
+  describe('PhoneListCtrl', function() {
+
+  it('should create "phones" model with phones', function() {
+    car scope ={},
+	ctrl = new PhoneListCtrl(scope);
+	
+	expect(scope.phones.length).toBe(3);
+  });
 
 });
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,11 +1,25 @@
 'use strict';
+ 
+ /* jasmine specs for controllers go here */
+describe('PhoneCat controllers', function() {
 
-/* jasmine specs for controllers go here */
+  describe('PhoneListCtrl', function(){
+    var scope, ctrl;
 
-describe('controllers', function() {
+    beforeEach(module('phonecatApp'));
 
-  it("should do something", function() {
+    beforeEach(inject(function($controller) {
+      scope = {};
+      ctrl = $controller('PhoneListCtrl', {$scope:scope});
+    }));
 
+    it('should create "phones" model with 3 phones', function() {
+      expect(scope.phones.length).toBe(3);
+    });
+
+
+    it('should set the default value of orderProp model', function() {
+      expect(scope.orderProp).toBe('age');
+    });
   });
-
 });
